043824b41840ea8b59ad312f2c29f6238a6cfc51
Prajwalrayal <rayalprajwal82991@gmail.com>
2022-10-03 00:39:01 +0530

    improved functioning

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
+let bill={
+}
+
+
+
+
+
+
 let addel=document.getElementById("button-add");
+console.log(addel);
 addel.addEventListener("click",()=>{
+    const dv=document.createElement("div");
+    dv.setAttribute('class','form-div');
     const node1 = document.createElement("input");
-    node1.setAttribute('id','item')
+    node1.setAttribute('class','item')
+    node1.setAttribute('type','text')
     const node2= document.createElement("input");
-    node2.setAttribute('id','qty')
+    node2.setAttribute('class','qty')
+    node1.setAttribute('type','text')
     const node3 = document.createElement("input");
-    node3.setAttribute('id','price')
+    node3.setAttribute('class','price')
+    node1.setAttribute('type','text')
     const node4 = document.createElement("br");
     const node5 = document.createElement("br");
 
     // Create a text node:
-    const textnode = document.createTextNode("Water");
+    const textnode = document.createTextNode("");
     
     // Append the text node to the "li" node:
     node1.appendChild(textnode);
@@ -18,9 +32,24 @@ addel.addEventListener("click",()=>{
     node3.appendChild(textnode);
     node4.appendChild(textnode);
     node5.appendChild(textnode);
-    document.getElementById("it").appendChild(node1);  
-    document.getElementById("it").appendChild(node2);  
-    document.getElementById("it").appendChild(node3);  
-    document.getElementById("it").appendChild(node4);  
-    document.getElementById("it").appendChild(node5);  
+    
+    dv.appendChild(node1);  
+    dv.appendChild(node2);  
+    dv.appendChild(node3);  
+    dv.appendChild(node4);  
+    dv.appendChild(node5);  
+    document.getElementById("it").appendChild(dv);
+})
+
+
+
+
+
+
+
+let doEL=document.getElementById("done")
+doEL.addEventListener("click",()=>{
+  document.querySelectorAll('.form-div input').forEach((el) => {
+    console.log(el.value)
+  })
 })
