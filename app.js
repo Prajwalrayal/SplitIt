402caeda287ae24bd39e852afc8f8cb76af48b8f
Prajwalrayal <rayalprajwal82991@gmail.com>
2022-10-03 01:08:56 +0530

    collect data from page

--- app.js
+++ app.js
@@ -1,55 +1,50 @@
-let bill={
-}
+let data = [];
 
-
-
-
-
-
-let addel=document.getElementById("button-add");
+let addel = document.getElementById("button-add");
 console.log(addel);
-addel.addEventListener("click",()=>{
-    const dv=document.createElement("div");
-    dv.setAttribute('class','form-div');
-    const node1 = document.createElement("input");
-    node1.setAttribute('class','item')
-    node1.setAttribute('type','text')
-    const node2= document.createElement("input");
-    node2.setAttribute('class','qty')
-    node1.setAttribute('type','text')
-    const node3 = document.createElement("input");
-    node3.setAttribute('class','price')
-    node1.setAttribute('type','text')
-    const node4 = document.createElement("br");
-    const node5 = document.createElement("br");
+addel.addEventListener("click", () => {
+  const dv = document.createElement("div");
+  dv.setAttribute("class", "form-div");
+  const node1 = document.createElement("input");
+  node1.setAttribute("class", "item");
+  node1.setAttribute("type", "text");
+  const node2 = document.createElement("input");
+  node2.setAttribute("class", "qty");
+  node1.setAttribute("type", "text");
+  const node3 = document.createElement("input");
+  node3.setAttribute("class", "price");
+  node1.setAttribute("type", "text");
+  const node4 = document.createElement("br");
+  const node5 = document.createElement("br");
 
-    // Create a text node:
-    const textnode = document.createTextNode("");
-    
-    // Append the text node to the "li" node:
-    node1.appendChild(textnode);
-    node2.appendChild(textnode);
-    node3.appendChild(textnode);
-    node4.appendChild(textnode);
-    node5.appendChild(textnode);
-    
-    dv.appendChild(node1);  
-    dv.appendChild(node2);  
-    dv.appendChild(node3);  
-    dv.appendChild(node4);  
-    dv.appendChild(node5);  
-    document.getElementById("it").appendChild(dv);
-})
+  // Create a text node:
+  const textnode = document.createTextNode("");
 
+  // Append the text node to the "li" node:
+  node1.appendChild(textnode);
+  node2.appendChild(textnode);
+  node3.appendChild(textnode);
+  node4.appendChild(textnode);
+  node5.appendChild(textnode);
 
+  dv.appendChild(node1);
+  dv.appendChild(node2);
+  dv.appendChild(node3);
+  dv.appendChild(node4);
+  dv.appendChild(node5);
+  document.getElementById("it").appendChild(dv);
+});
 
+let doEL = document.getElementById("done");
+doEL.addEventListener("click", () => {
+  document.querySelectorAll(".form-div").forEach((el) => {
+    obj = {
+      "item-name": el.querySelector(".item").value,
+      "quantity": el.querySelector(".qty").value,
+      "price": el.querySelector(".price").value,
+    };
+    data.push(obj);
 
-
-
-
-let doEL=document.getElementById("done")
-doEL.addEventListener("click",()=>{
-  document.querySelectorAll('.form-div input').forEach((el) => {
-    console.log(el.value)
-  })
-})
+  });
+  console.log(data);
+});
