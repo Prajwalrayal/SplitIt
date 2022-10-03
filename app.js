583f4818ba3f8db61ade8c363e5879a658198a2c
Prajwalrayal <rayalprajwal82991@gmail.com>
2022-10-03 11:38:28 +0530

    improve code structure

--- app.js
+++ app.js
@@ -1,50 +1,68 @@
-let data = [];
+let data = []; //array initialization of form data
 
-let addel = document.getElementById("button-add");
-console.log(addel);
-addel.addEventListener("click", () => {
-  const dv = document.createElement("div");
-  dv.setAttribute("class", "form-div");
-  const node1 = document.createElement("input");
-  node1.setAttribute("class", "item");
-  node1.setAttribute("type", "text");
-  const node2 = document.createElement("input");
-  node2.setAttribute("class", "qty");
-  node1.setAttribute("type", "text");
-  const node3 = document.createElement("input");
-  node3.setAttribute("class", "price");
-  node1.setAttribute("type", "text");
-  const node4 = document.createElement("br");
-  const node5 = document.createElement("br");
 
-  // Create a text node:
-  const textnode = document.createTextNode("");
-
-  // Append the text node to the "li" node:
-  node1.appendChild(textnode);
-  node2.appendChild(textnode);
-  node3.appendChild(textnode);
-  node4.appendChild(textnode);
-  node5.appendChild(textnode);
-
-  dv.appendChild(node1);
-  dv.appendChild(node2);
-  dv.appendChild(node3);
-  dv.appendChild(node4);
-  dv.appendChild(node5);
-  document.getElementById("it").appendChild(dv);
-});
-
-let doEL = document.getElementById("done");
-doEL.addEventListener("click", () => {
+//Function to get data of items through form
+function get_data()   
+{
   document.querySelectorAll(".form-div").forEach((el) => {
-    obj = {
+    // Creating object of data recieved
+    obj = {                                        
       "item-name": el.querySelector(".item").value,
       "quantity": el.querySelector(".qty").value,
       "price": el.querySelector(".price").value,
     };
+    // pushing object to data array
     data.push(obj);
-
   });
+}
+
+
+// creating an element
+function add_node(class_name)
+{
+  const node=document.createElement("input");
+  node.setAttribute("class", class_name);
+  node.setAttribute("type", "text");
+  return node;
+}
+
+// Creating input field
+function create_form()
+{
+  const dv = document.createElement("div");
+  dv.setAttribute("class", "form-div");
+
+  const node1 = add_node("item","text");
+  const node2 = add_node("qty",);
+  const node3 = add_node("price");
+
+  // Create a text node:
+  const textnode = document.createTextNode("");
+
+
+  let node=[node1,node2,node3];
+  // Append the text node:
+  for(let i=0;i<3;i++)
+  {
+    node[i].appendChild(textnode);
+  }
+
+  for(let i=0;i<3;i++)
+  {
+    dv.appendChild(node[i]);
+  }
+  document.getElementById("it").appendChild(dv);
+}
+
+// Functioning of add item button
+let addel = document.getElementById("button-add");
+addel.addEventListener("click", () => {
+      create_form();
+});
+// Functioning of Done button
+let doEL = document.getElementById("done");
+doEL.addEventListener("click", () => {
+  get_data();
   console.log(data);
+  data=[];
 });
