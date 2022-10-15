cf616d1b90c334e1e32678f81eb18cb0f84f1de0
Prajwalrayal <rayalprajwal82991@gmail.com>
2022-10-15 14:54:22 +0530

    persistent form data

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
-
-
-
 let data = [{
 }]; //array initialization of form data
+
+
+
+function total_amt()
+{
+  let sum=0;
+  for( let i=0 ; i < data.length ; i++)
+  {
+    let copy=data[i];
+    let qt=parseInt(copy.quantity);
+    let pri=parseInt(copy.price);
+    if(isNaN(qt) || isNaN(pri))
+      {
+        continue;
+      }
+    sum+=(qt*pri);
+  }
+  data=[];
+  let tothtml=document.getElementById("total");
+  tothtml.textContent=("Total Bill:  "+sum);
+}
+
+
+function fill_data(dv,cls,item)
+{ 
+    cls='.'+cls
+    // console.log(item);
+    dv.querySelector(cls).value=item
+}
+
+
+if(localStorage.getItem("form-data"))
+{
+  JSON.parse(localStorage.getItem("form-data")).forEach((item) => {
+    const dv = create_form();
+    data.push(item);
+    fill_data(dv,"item",item["item-name"])
+    fill_data(dv,"qty",item["quantity"])
+    fill_data(dv,"price",item["price"])
+    total_amt()
+  })
+}
+
+
   
 
 
@@ -57,41 +98,29 @@ function create_form()
     dv.appendChild(node[i]);
   }
   document.getElementById("it").appendChild(dv);
+  return dv;
 }
 
+let inpEl;
 // Functioning of add item button
 let addel = document.getElementById("button-add");
 addel.addEventListener("click", () => {
       create_form();
+      inpEl=document.querySelectorAll("input")
+      listen(inpEl)
 });
-
-
-//Totalling of price
-let totEl=document.getElementById("button-tot");
 
 // Event Listner
-totEl.addEventListener("click",()=>{
-
-      get_data();
-      let sum=0;
-      for( let i=0 ; i < data.length ; i++)
-      {
-        let copy=data[i];
-        let qt=parseInt(copy.quantity);
-        let pri=parseInt(copy.price);
-        if(isNaN(qt) || isNaN(pri))
-          {
-            continue;
-          }
-        sum+=(qt*pri);
-        // console.log(qt);
-        // console.log(pri);
-      }
-      data=[];
-      let tothtml=document.getElementById("total");
-      tothtml.textContent=("Total Bill:  "+sum);
-
-});
+function listen(inpEl) {
+  inpEl.forEach((ele) => {
+    //console.log(ele);
+    ele.addEventListener("change",()=> {
+          get_data();
+          localStorage.setItem('form-data',JSON.stringify(data))
+          total_amt();
+        })
+  });
+}
 
 
 // Functioning of Done button
@@ -107,3 +136,11 @@ doEL.addEventListener("click", () => {
 });
 // module.exports={data};
 // export const message = data;
+
+let resEl=document.getElementById("button-res")
+
+resEl.addEventListener("click",()=>{
+  localStorage.removeItem("form-data")
+  document.getElementById("it").innerHTML=""
+  document.getElementById("total").innerHTML=""
+})
